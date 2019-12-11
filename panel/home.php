a8eee58374953017202390a08eb1f2cdaa89e9af
actuallyakash <akash_gupta@hotmail.com>
2019-12-11 17:41:46 +0530

    Updated home contents

--- panel/home.php
+++ panel/home.php
@@ -13,30 +13,52 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 
 <div id="general" class="ma-tabs-content">
-    <div class="ma-row ma-tabs-content-wrap">
+    <div class="mt-banner">
+        <img class="banner" src="<?php echo MIGHTY_ADDONS_PLG_URL . 'assets/admin/images/banner.jpg'; ?>" alt="">
+    </div>
+    <div class="ma-row ma-tabs-content-wrap container">
         <div class="ma-tabs-content-inner">
             <div class="ma-tabs-content-block-wrap">
-                <!-- Documentation Block -->
                 <div class="ma-tabs-block ma-tabs-block-docs">
                     <header class="ma-tabs-block-header">
-                        <h4 class="ma-tabs-block-title">Documentation</h4>
+                        <h4 class="ma-tabs-block-title">Need Help?</h4>
                     </header>
                     <div class="ma-tabs-block-content">
-                        <p>Get started by spending some time with the documentation to get familiar with Essential Addons. Build awesome websites for you or your clients with ease.</p>
-                        <a href="#" class="ma-btn" target="_blank">Documentation</a>
+                        <p>Facing issues while using addons? Get help from our dedicated WordPress Developers on <a href="https://mightythemes.com/support/c/mighty-addons/" target="_blank">Mighty Addons Official Forum</a>. Or join our <a target="_blank" href="https://www.facebook.com/groups/mightythemes/">Facebook Community</a> and post your questions there as well.</p>
+                        <a href="https://mightythemes.com/support/c/mighty-addons/" class="ma-btn" target="_blank">Reach out on Forum</a>
                     </div>
                 </div>
 
-                <!-- Documentation Block -->
                 <div class="ma-tabs-block ma-tabs-block-docs">
                     <header class="ma-tabs-block-header">
-                        <h4 class="ma-tabs-block-title">Documentation</h4>
+                        <h4 class="ma-tabs-block-title">Missing Any Feature?</h4>
                     </header>
                     <div class="ma-tabs-block-content">
-                        <p>Get started by spending some time with the documentation to get familiar with Essential Addons. Build awesome websites for you or your clients with ease.</p>
-                        <a href="#" class="ma-btn" target="_blank">Documentation</a>
+                        <p>Are we missing any features that you need for your project? Submit feature requests on GitHub and help us improve Mighty Addons.</p>
+                        <a href="https://github.com/mightythemes/Mighty-Addons/issues/new?template=issue_template.md&title=Feature%20Request:&labels=Feature%20Request" class="ma-btn" target="_blank">Request a Feature</a>
                     </div>
                 </div>
+
+                <div class="ma-tabs-block ma-tabs-block-docs">
+                    <header class="ma-tabs-block-header">
+                        <h4 class="ma-tabs-block-title">Developer? Want to Contribute?</h4>
+                    </header>
+                    <div class="ma-tabs-block-content">
+                        <p>We 💗 Open Source. You can contribute to MightyAddons by reporting bugs, creating issues and pull requests on GitHub.</p>
+                        <a href="https://github.com/mightythemes/Mighty-Addons/issues/new?template=issue_template.md&title=&labels=bug" class="ma-btn" target="_blank">Report a Bug</a>
+                    </div>
+                </div>
+
+                <div class="ma-tabs-block ma-tabs-block-docs">
+                    <header class="ma-tabs-block-header">
+                        <h4 class="ma-tabs-block-title">Loved it? Show Your Love.</h4>
+                    </header>
+                    <div class="ma-tabs-block-content">
+                        <p>Thanks for choosing Mighty Addons plugin. We are making it awesome day by day. That being said, if you could take a minute to post a review, we would so appreciate it.</p>
+                        <a href="https://wordpress.org/support/plugin/mighty-addons/reviews/#new-post" class="ma-btn" target="_blank">Yeah, you deserve 5 Stars</a>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
