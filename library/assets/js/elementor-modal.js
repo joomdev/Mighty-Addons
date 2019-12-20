3effc9daf9912bf377f67602a0cbe4defee1b2e6
Akash <akash_gupta@hotmail.com>
2019-12-20 19:54:35 +0530

    Button Added for Library

--- library/assets/js/elementor-modal.js
+++ library/assets/js/elementor-modal.js
@@ -1,11 +1,18 @@
 /* global elementor, elementorCommon */
 /* eslint-disable */
-"undefined" != typeof jQuery &&
-	!(function($) {
-		$(function() {
-		
-
+"undefined" != typeof jQuery && function ( $ ) {
+    $(function () {
+        function showStuff() {
+			alert('mighty library');
 			
-			}
-		});
-	})(jQuery);
+        }
+        window.elementsModal = null;
+        var n = $("#tmpl-elementor-add-section");
+        if (0 < n.length) {
+            var t = n.text();
+            t = t.replace('<div class="elementor-add-section-drag-title', '<div class="elementor-add-section-area-button elementor-add-mighty-button" title="Mighty Library"> <i class="fa fa-folder"></i> </div><div class="elementor-add-section-drag-title'), n.text(t), elementor.on("preview:loaded", function () {
+                $(elementor.$previewContents[0].body).on("click", ".elementor-add-mighty-button", showStuff)
+            })
+        }
+    })
+}(jQuery);
--- library/inc/class-elementor.php
+++ library/inc/class-elementor.php
@@ -20,9 +20,10 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @since 1.2.1
  */
-class Elementor {
+class Elementor extends base {
     
 	public function __construct() {
+		
 		add_action( 'elementor/editor/before_enqueue_scripts', [ $this, 'enqueue_editor_scripts' ] );
 		add_action( 'elementor/preview/enqueue_styles', [ $this, 'enqueue_editor_scripts' ] );
 
@@ -30,45 +31,13 @@ class Elementor {
 
 	/**
 	 * Load styles and scripts for Elementor modal.
-	 *
-	 * @return void
 	 */
 	public function enqueue_editor_scripts() {
 
-        wp_enqueue_script( 'mightyaddons-elementor-modal', MIGHTY_ADDONS_PLG_URL . 'assets/js/elementor-modal.js', [ 'jquery' ], MIGHTY_ADDONS_VERSION, false );
-        
-		wp_enqueue_style( 'mightyaddons-elementor-modal', MIGHTY_ADDONS_PLG_URL . 'assets/css/elementor-modal.css', [ 'dashicons' ], MIGHTY_ADDONS_VERSION );
+		wp_enqueue_script( 'mightyaddons-elementor-modal', MIGHTY_ADDONS_PLG_URL . 'library/assets/js/elementor-modal.js', [ 'jquery' ], MIGHTY_ADDONS_VERSION );
+		
+		wp_enqueue_style( 'mightyaddons-elementor-modal', MIGHTY_ADDONS_PLG_URL . 'library/assets/css/elementor-modal.css', [], MIGHTY_ADDONS_VERSION );
 
-		wp_enqueue_script(
-			'mightylibrary-modal',
-			MIGHTY_ADDONS_PLG_URL . 'assets/js/app-modal.js',
-			[
-				'react',
-				'react-dom',
-				'jquery',
-				'wp-components',
-				'wp-hooks',
-				'wp-i18n',
-				'wp-api-fetch',
-				'wp-html-entities',
-			],
-			MIGHTY_ADDONS_VERSION,
-			true
-		);
-		wp_set_script_translations( 'mightylibrary-modal', 'ang' );
-
-		wp_enqueue_style( 'wp-components' );
-
-		$i10n = apply_filters( // phpcs:ignore
-			'analog/app/strings',
-			[
-				'is_settings_page' => false,
-				'syncColors'       => ( '' !== Options::get_instance()->get( 'ang_sync_colors' ) ? Options::get_instance()->get( 'ang_sync_colors' ) : true ),
-				'stylekit_queue'   => Utils::get_stylekit_queue() ? array_values( Utils::get_stylekit_queue() ) : [],
-			]
-		);
-
-		wp_localize_script( 'mightylibrary-modal', 'AGWP', $i10n );
 	}
 }
 
