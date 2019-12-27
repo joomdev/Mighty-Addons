4f16979c54ca40fbfa5019d1417e57e764258a42
Akash <akash_gupta@hotmail.com>
2019-12-27 18:50:54 +0530

    Bifurcation and fixed css issue

--- library/assets/js/lib2/src/components/app.jsx
+++ library/assets/js/lib2/src/components/app.jsx
@@ -1,41 +1,20 @@
-import React, { Component } from 'react';
-import '../styles/grid.min.css';
-import '../styles/mt.css';
+import React, { Component } from 'react'
+
+import Header from "./Header"
+import Kit from "./Kit"
+
+import '../styles/grid.min.css'
+import '../styles/mt.css'
 
 class App extends Component {
 
   render() {
     return (
-      <div id="mt-templates-modal" className="mt-templates-modal">
+      <div id="mt-templates-modal">
         <div className="mt-templates-modal-inner mt-container">
-          <div className="mt-templates-modal-header mt-row">
-            <div className="mt-col-sm-4">
-              <div className="brand-logo">
-                <img src="images/logo-white-1.png" alt="Mighty Logo" />
-              </div>
-            </div>
-            <div className="mt-col-sm-4">
-              <div className="mt-templates-modal-header-top-tabs">
-                <ul className="top-tabs-inner">
-                  <li className="top-tabs-temp active">Templates <span className="top-tabs-numb">5</span></li>
-                  <li className="top-tabs-kits">Blocks <span className="top-tabs-numb">200</span></li>
-                </ul>
-              </div>
-            </div>
-            <div className="mt-col-sm-4">
-              <div className="mt-templates-modal-header-top-right">
-                <ul className="top-right">
-                  <li className="top-right-list">
-                    <span>Sync</span>
-                    <div className="top-sync"><img className="icon" src="images/sync-solid.svg" alt="" /></div>
-                  </li>
-                  <li className="top-right-list">
-                    <div className="top-close icon"><img className="icon" src="images/times-solid.svg" alt="" /></div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+
+          <Header />
+          
           <div className="mt-templates-modal-body">
             <div className="mt-templates-modal-body-inner">
               {/* MT Templates Modal Header */}
@@ -88,69 +67,17 @@ class App extends Component {
                   </div>
                 </div>
               </div>
-              {/* MT Templates Modal Main Body */}
+              
               <div className="mt-templates-modal-body-main">
                 <div className="template-item">
-                  <div className="template-item-inner">
-                    <ul className="template-btn-group">
-                      <li className="template-btn-item mt-btn mt-btn-preview">
-                        <span><img className="icon" src="images/eye-solid.svg" alt="" /></span>
-                      </li>
-                      <li className="template-btn-item mt-btn mt-btn-go">
-                        <span><img className="icon" src="images/external-link-alt-solid.svg" alt="" /> Go
-                          Pro</span>
-                      </li>
-                      <li className="template-btn-item mt-btn mt-btn-fav">
-                        <span><img className="icon" src="images/heart-solid.svg" alt="" /></span>
-                      </li>
-                    </ul>
-                    <div className="template-item-figure">
-                      <img src="images/team-view-1.jpg" alt="" />
-                    </div>
-                    <div className="template-item-name"><span>Jd Builder</span>6 Pages</div>
-                  </div>
-                  <div className="template-item-inner">
-                    <ul className="template-btn-group">
-                      <li className="template-btn-item mt-btn mt-btn-preview">
-                        <span><img className="icon" src="images/eye-solid.svg" alt="" /></span>
-                      </li>
-                      <li className="template-btn-item mt-btn mt-btn-go">
-                        <span><img className="icon" src="images/external-link-alt-solid.svg" alt="" /> Go
-                          Pro</span>
-                      </li>
-                      <li className="template-btn-item mt-btn mt-btn-fav">
-                        <span><img className="icon" src="images/heart-solid.svg" alt="" /></span>
-                      </li>
-                    </ul>
-                    <div className="template-item-figure">
-                      <img src="images/team-view-2.jpg" alt="" />
-                    </div>
-                    <div className="template-item-name"><span>Jd Builder</span>4 Pages</div>
-                  </div>
-                  <div className="template-item-inner">
-                    <ul className="template-batch">
-                      <li className="template-batch-item template-batch-letest"><span>Letest</span></li>
-                      <li className="template-batch-item template-batch-pro"><span>Pro</span></li>
-                    </ul>
-                    <ul className="template-btn-group">
-                      <li className="template-btn-item mt-btn mt-btn-preview">
-                        <span><img className="icon" src="images/eye-solid.svg" alt="" /></span>
-                      </li>
-                      <li className="template-btn-item mt-btn mt-btn-go">
-                        <span><img className="icon" src="images/external-link-alt-solid.svg" alt="" /> Go
-                          Pro</span>
-                      </li>
-                      <li className="template-btn-item mt-btn mt-btn-fav">
-                        <span><img className="icon" src="images/heart-solid.svg" alt="" /></span>
-                      </li>
-                    </ul>
-                    <div className="template-item-figure">
-                      <img src="images/team-view-3.jpg" alt="" />
-                    </div>
-                    <div className="template-item-name"><span>Jd Builder</span>8 Pages</div>
-                  </div>
+                  <Kit />
+                  <Kit />
+                  <Kit />
+                  <Kit />
+                  <Kit />
                 </div>
               </div>
+
             </div>
           </div>
         </div>
@@ -160,4 +87,4 @@ class App extends Component {
 
 }
 
-export default App;
+export default App
