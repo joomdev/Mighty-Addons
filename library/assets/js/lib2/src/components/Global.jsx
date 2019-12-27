9050366f744d65b6caa70c6090accceb6b045294
Akash <akash_gupta@hotmail.com>
2019-12-27 18:50:54 +0530

    Bifurcation and fixed css issue

---
+++ library/assets/js/lib2/src/components/Global.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+import '../styles/grid.min.css'
+import '../styles/mt.css'
+
+import Header from "./Header"
+
+class Global extends Component {
+    
+}
+
+export default Global;
