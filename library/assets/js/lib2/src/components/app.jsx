1f144dc28b3b04a571b8846c2c5b70b45ce545f8
Akash <akash_gupta@hotmail.com>
2019-12-30 18:46:24 +0530

    Added Dynamic View  on setState

--- library/assets/js/lib2/src/components/app.jsx
+++ library/assets/js/lib2/src/components/app.jsx
@@ -10,9 +10,8 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      view: 'templates', // template
+      renderView: 'home', // template
       items: [],
-      _data: new Map()
     };
   }
 
@@ -21,16 +20,10 @@ class App extends Component {
       .then(res => res.json())
       .then(
         (result) => {
-          console.log(result);
-          var _items = new Map();
-          result.kits.forEach(function(_kit){
-            _items.set('template-' + _kit.id, _kit);
-          });
 
           this.setState({
             isLoaded: true,
-            items: result.kits,
-            _data: _items,
+            items: result.kits
           });
         },
         // Note: it's important to handle errors here
@@ -45,13 +38,19 @@ class App extends Component {
       )
   }
 
-  showTemplate(_id){
-    // console.log(_id);
-    var _template = this._data.get(_id);
-    // this.setState({'view': 'template'});
+  handleClick = ( view ) => {
+    this.setState( {view});
   }
-  back(){
-    // this.setState({'view': 'templates'});
+
+  createView() {
+    
+    switch( this.state.renderView ) {
+      case 'home':
+        return <Kits data={this.state.items} />;
+      case 'templates':
+        return <Templates />
+    }
+
   }
 
   render() {
@@ -62,11 +61,7 @@ class App extends Component {
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
-
-      items.forEach(item => {
-        console.log(item);
-      });
-
+      
       return (
         
         <div id="mt-templates-modal">
@@ -102,96 +97,115 @@ class App extends Component {
                   </div>
               </div>
 
-              <div className="mt-templates-modal-body">
-                  <div className="mt-templates-modal-body-inner">
-
-                      <div className="mt-templates-modal-body-header mt-row">
-                          <div className="mt-col-sm-6">
-                              <div className="body-header-left">
-                                  <div className="filter body-header-filter">
-                                      <label htmlFor="filter">Filter</label>
-                                      <div className="filter-dropdown">
-                                          <div className="filter-dropdown-inner">
-                                              <div className="filter-dropdown-inner-left">
-                                                  <div className="filter-dropdown-inner-left-text">Show All</div>
-                                                  <input id="filter-list" readOnly tabIndex={0} className="filter-input"
-                                                      defaultValue />
-                                              </div>
-                                              <div className="filter-dropdown-inner-right">
-                                                  <span><img className="icon" src="images/angle-down-solid.svg"
-                                                          alt="" /></span>
-                                              </div>
-                                          </div>
-                                          <div className="filter-dropdown-list">
-                                              <ul className="filter-dropdown-list-inner">
-                                                  <li className="filter-dropdown-list-items active"><a href="#">Show all</a>
-                                                  </li>
-                                                  <li className="filter-dropdown-list-items"><a href="#">Page</a></li>
-                                                  <li className="filter-dropdown-list-items"><a href="#">Single</a></li>
-                                                  <li className="filter-dropdown-list-items"><a href="#">Archive</a></li>
-                                              </ul>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                          <div className="mt-col-sm-6">
-                              <div className="body-header-search">
-                                  <input type="text" placeholder="Search Templates.." name="search" />
-                                  <button type="submit">
-                                      <img className="icon" src="images/search-solid.svg" alt="" />
-                                  </button>
-                              </div>
-                          </div>
-                      </div>
-
-                      <div className="mt-templates-modal-body-mid mt-row">
-                          <div className="mt-col-sm-6">
-                              <div className="mt-templates-modal-body-mid-left">Sign up for an exclusive launch discount.
-                              </div>
-                          </div>
-                          <div className="mt-col-sm-6">
-                              <div className="mt-templates-modal-body-mid-right">
-                                  <a href="#" className="mt-btn mt-btn-blue">Learn more</a>
-                              </div>
-                          </div>
-                      </div>
-
-                      <div className="mt-templates-modal-body-main">
-                          <div className="template-item">
-
-                              {items.map(item => (
-                                <div className="template-item-inner">
-                                  <ul className="template-btn-group">
-                                      <li className="template-btn-item mt-btn mt-btn-preview">
-                                          <span onClick={this.showTemplate(item.name)}><img className="icon" src="images/eye-solid.svg" alt="" /></span>
-                                      </li>
-                                      <li className="template-btn-item mt-btn mt-btn-go">
-                                          <span><img className="icon" src="images/external-link-alt-solid.svg" alt="" />Go Pro</span>
-                                      </li>
-                                      <li className="template-btn-item mt-btn mt-btn-fav">
-                                          <span><img className="icon" src="images/heart-solid.svg" alt="" /></span>
-                                      </li>
-                                  </ul>
-                                  <div className="template-item-figure">
-                                      <img src={item.image} alt="" />
-                                  </div>
-                                  <div className="template-item-name"><span>{item.name}</span>{item.pages} Pages</div>
-                                </div>
-
-                              ))}
-                          </div>
-                      </div>
-
-                  </div>
-              </div>
+              {this.createView()}
+              
           </div>
-      </div>
+        </div>
       );
     }
   
   }
 
+}
+
+class Kits extends Component {
+  
+  render() {
+    return (
+      <div className="mt-templates-modal-body">
+        <div className="mt-templates-modal-body-inner">
+
+              <div className="mt-templates-modal-body-header mt-row">
+                  <div className="mt-col-sm-6">
+                      <div className="body-header-left">
+                          <div className="filter body-header-filter">
+                              <label htmlFor="filter">Filter</label>
+                              <div className="filter-dropdown">
+                                  <div className="filter-dropdown-inner">
+                                      <div className="filter-dropdown-inner-left">
+                                          <div className="filter-dropdown-inner-left-text">Show All</div>
+                                          <input id="filter-list" readOnly tabIndex={0} className="filter-input"
+                                              defaultValue />
+                                      </div>
+                                      <div className="filter-dropdown-inner-right">
+                                          <span><img className="icon" src="images/angle-down-solid.svg"
+                                                  alt="" /></span>
+                                      </div>
+                                  </div>
+                                  <div className="filter-dropdown-list">
+                                      <ul className="filter-dropdown-list-inner">
+                                          <li className="filter-dropdown-list-items active"><a href="#">Show all</a>
+                                          </li>
+                                          <li className="filter-dropdown-list-items"><a href="#">Page</a></li>
+                                          <li className="filter-dropdown-list-items"><a href="#">Single</a></li>
+                                          <li className="filter-dropdown-list-items"><a href="#">Archive</a></li>
+                                      </ul>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="mt-col-sm-6">
+                      <div className="body-header-search">
+                          <input type="text" placeholder="Search Templates.." name="search" />
+                          <button type="submit">
+                              <img className="icon" src="images/search-solid.svg" alt="" />
+                          </button>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="mt-templates-modal-body-mid mt-row">
+                  <div className="mt-col-sm-6">
+                      <div className="mt-templates-modal-body-mid-left">Sign up for an exclusive launch discount.
+                      </div>
+                  </div>
+                  <div className="mt-col-sm-6">
+                      <div className="mt-templates-modal-body-mid-right">
+                          <a href="#" className="mt-btn mt-btn-blue">Learn more</a>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="mt-templates-modal-body-main">
+                  <div className="template-item">
+
+                      {this.props.data.map(item => (
+                        <div className="template-item-inner">
+                          <ul className="template-btn-group">
+                              <li className="template-btn-item mt-btn mt-btn-preview">
+                                  <span onClick={ () => { this.handleClick(item.templates) } }>View</span>
+                              </li>
+                              <li className="template-btn-item mt-btn mt-btn-go">
+                                  <span><img className="icon" src="images/external-link-alt-solid.svg" alt="" />Go Pro</span>
+                              </li>
+                              <li className="template-btn-item mt-btn mt-btn-fav">
+                                  <span><img className="icon" src="images/heart-solid.svg" alt="" /></span>
+                              </li>
+                          </ul>
+                          <div className="template-item-figure">
+                              <img src={item.image} alt="" />
+                          </div>
+                          <div className="template-item-name"><span>{item.name}</span>{item.pages} Pages</div>
+                        </div>
+
+                      ))}
+                  </div>
+              </div>
+
+          </div>
+      </div>
+    );
+  }
+}
+
+class Templates extends Component {
+  
+  render() {
+    return (
+      <h1>Here comes the Templates component!!</h1>
+    );
+  }
 }
 
 export default App
