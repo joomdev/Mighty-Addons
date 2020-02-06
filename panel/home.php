cca8fe7be10b3219272a64f5c766e73550890f25
Akash <akash_gupta@hotmail.com>
2020-02-06 19:00:12 +0530

    ➕Key page 🔑

--- panel/home.php
+++ panel/home.php
@@ -18,6 +18,12 @@ $extensions = self::$ma_get_settings['extensions'];
 
 <?php include_once('includes/extension-settings.php'); ?>
 
-<?php include_once('includes/go-pro.php'); ?>
+<?php
+if( !is_plugin_active( 'Mighty-Addons-Pro/mighty-addons-pro.php' ) ) {
+    include_once('includes/go-pro.php');
+} else {
+    include_once(MIGHTY_ADDONS_PRO_DIR_PATH . 'pages/go-pro.php');
+}
+?>
 
 <?php include_once('includes/footer.php'); ?>
