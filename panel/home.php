e1bce7fbadada1533549c5ebb1f72dc36fe1371b
Akash <akash_gupta@hotmail.com>
2020-03-17 17:59:51 +0530

    Fixed Condition 🐛

--- panel/home.php
+++ panel/home.php
@@ -30,10 +30,10 @@ if ( in_array( 'Mighty-Addons-Pro/mighty-addons-pro.php', $allPlugins['active'],
 <?php include_once('includes/extension-settings.php'); ?>
 
 <?php
-if( !is_plugin_active( 'Mighty-Addons-Pro/mighty-addons-pro.php' ) ) {
-    include_once('includes/go-pro.php');
-} else {
+if( is_plugin_active( 'Mighty-Addons-Pro/mighty-addons-pro.php' ) || is_plugin_active( 'mighty-addons-pro/mighty-addons-pro.php' )) {
     include_once(MIGHTY_ADDONS_PRO_DIR_PATH . 'pages/go-pro.php');
+} else {
+    include_once('includes/go-pro.php');
 }
 ?>
 
