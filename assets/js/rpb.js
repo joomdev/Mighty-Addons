53d1f05cd03e1709e646c9282e251797c38c516d
Akash <akash_gupta@hotmail.com>
2021-04-09 15:45:46 +0530

    [RPB} Fixed BTT progress bar ⭕

--- assets/js/rpb.js
+++ assets/js/rpb.js
@@ -3,10 +3,49 @@
 
   window.onscroll = function() {readingProgressBar()};
 
-  function readingProgressBar() {
+  // Configuration of Progress bar
+  if( document.getElementById( "ma-rpb" ) ) {
+    
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
+    var scrolled = ( winScroll / height ) * 100;
+
+  } else if ( document.getElementById( "ma-btt-rpb" ) ) {
+      
+    var progressPath = document.querySelector( '.ma-progress-wrap path' );
+    var pathLength = progressPath.getTotalLength();
+
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+    progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+
+    let offset = 50;
+    let duration = 550;
+
+    // Activate progress bar after defined limit
+    $( window ).on( 'scroll', function() {
+      if ( $(this).scrollTop() > offset ) {
+        $( '.ma-progress-wrap' ).addClass( 'active-progress' );
+      } else {
+        $( '.ma-progress-wrap' ).removeClass( 'active-progress' );
+      }
+    });
+
+    // Send to top
+    $( '.ma-progress-wrap' ).on( 'click', function( event ) {
+      event.preventDefault();
+      $( 'html, body' ).animate({
+        scrollTop: 0
+      }, duration );
+      return false;
+    })
+
+  }
+
+  // Updating progress
+  function readingProgressBar() {
 
     // View 1
     if( document.getElementById( "ma-rpb" ) ) {
@@ -15,39 +54,12 @@
 
     // View 2
     if ( document.getElementById( "ma-btt-rpb" ) ) {
-      
-      let progressPath = document.querySelector('.ma-progress-wrap path');
-      let pathLength = progressPath.getTotalLength();
-      let progress = pathLength - (winScroll * pathLength / height);
 
-      progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
-      progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
-      progressPath.style.strokeDashoffset = pathLength;
-      progressPath.getBoundingClientRect();
-      progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
-      
+      var scroll = $(window).scrollTop();
+      var height = $(document).height() - $(window).height();
+      var progress = pathLength - (scroll * pathLength / height);
+      console.log('scrolled', progress);
       progressPath.style.strokeDashoffset = progress;
-
-      let offset = 50;
-      let duration = 550;
-
-      // Activate progress bar after defined limit
-      $(window).on('scroll', function() {
-        if ($(this).scrollTop() > offset) {
-          $('.ma-progress-wrap').addClass('active-progress');
-        } else {
-          $('.ma-progress-wrap').removeClass('active-progress');
-        }
-      });
-
-      // Send to top
-      $('.ma-progress-wrap').on('click', function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, duration);
-        return false;
-      })
     }
 
   }
