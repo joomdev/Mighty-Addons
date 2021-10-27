8cd1b1ca8388c3670c6fd89a5491b3be0d0dacf7
jyotibansal461 <47344845+jyotibansal461@users.noreply.github.com>
2021-10-27 22:44:02 +0530

    set css property

--- classes/class-age-checker.php
+++ classes/class-age-checker.php
@@ -17,13 +17,15 @@ class AgeChecker{
         $title = ( $settings[ 'enable_title' ] == 'yes' ) ? $settings[ 'title' ] : '';
 
         $description = ( $settings[ 'enable_description' ] == 'yes' ) ? $settings[ 'description' ] : '';
-
     
     ?>
         <div class="ma-agech">
-            <div class="ma-agech__wrapper ma-right-side-image default">
+
+            <div class="ma-agech__wrapper ma-right-side-image <?php echo $settings['display_position'];?> ">
+
                 <div class="ma-agech__content">
-                    <div class="ma-agech__content-inner ma-align-center">
+
+                    <div class="ma-agech__content-inner ma-align-<?php echo $settings['content_alignment'];?>">
 
                         <?php if ( $logo ) { ?>
                             <div class="ma-agech__logo-wrapper">
@@ -39,33 +41,48 @@ class AgeChecker{
                             <p class="ma-agech__description"><?php echo $description; ?></p>
                         <?php } ?>
 
-                        <div class="ma-agech__checkbox-wrapper">
-                            <input type="checkbox" id="age18plus" class="ma-agech__checkbox" name="age"
-                            value="age">
-                            <label for="vehicle1" class="ma-agech__checkbox-label">I confirm that i am 18 years old or over</label>
-                        </div>
+                        <?php if ( $settings['method'] == 'age_confirmation' ) { ?>
+                            <div class="ma-agech__checkbox-wrapper">
+                                <input type="checkbox" id="age18plus" class="ma-agech__checkbox" name="age"value="age">
+                                <label for="vehicle1" class="ma-agech__checkbox-label"><?php echo $settings['check_input_box'];?></label>
+                            </div>
+                        <?php } ?>
+
                         <div class="ma-agech__input-btn-wrapper">
+
                             <div class="ma-agech__input-wrapper">
                                 <input type="date" id="birthdate" class="ma-agech__input" name="birthdate">
                             </div>
+
                             <div class="ma-agech__btn-wrapper">
-                                <a href="#" class="ma-agech__btn-primary ma-agech__icon-before"
-                                    role="btn">
-                                        <span class="ma-agech-btn__icon"><i class="fas fa-check"></i></span>
-                                        <span class="ma-agech-btn__text">ENTER</span>
+
+                                <a href="#" class="ma-agech__btn-primary ma-agech__icon-<?php echo $settings['icon_position'];?>"role="btn">
+                                    <span class="ma-agech-btn__icon"><i class="<?php echo $settings['button_icon']['value']; ?>"></i></span>
+                                    <span class="ma-agech-btn__text"><?php echo $settings['button_text']; ?></span>
+                                    
                                 </a>
-                                <a href="#" class="ma-agech__btn-secondary ma-agech__icon-after"
-                                    role="btn">
-                                    <span class="ma-agech-btn__icon"><i class="fas fa-times"></i></span>
-                                    <span class="ma-agech-btn__text">No</span>
-                                </a>
+
+                                <?php if( $settings['method'] == 'yes_no' ) { ?>
+                                    <a href="#" class="ma-agech__btn-secondary ma-agech__icon-after"role="btn">
+                                        <span class="ma-agech-btn__icon"><i class="fas fa-times"></i></span>
+                                        <span class="ma-agech-btn__text">No</span>
+                                    </a>
+                                <?php } ?>
+
                             </div>
+
                         </div>
+
                     </div>
-                    <p class="ma-agech__bottom-text ma-align-center">By entering this site you are agreeing to the Terms of Use and Privacy Policy</p>
+
+                    <p class="ma-agech__bottom-text ma-align-<?php echo $settings['bottom_line_alignment'];?>"><?php echo $settings['bottom_text']; ?></p>
+
                 </div>
+
                 <div class="ma-agech__side-image"></div>
+
             </div>
+
 	    </div>
 
 	<script type="text/javascript"  src="https://apiv2.popupsmart.com/api/Bundle/371727" async></script>
