de5f484c906d4bd2e87a10252ad4b421115c95cb
jyotibansal461 <47344845+jyotibansal461@users.noreply.github.com>
2021-11-22 20:27:22 +0530

    only one chart is working issue solved

--- widgets/piedonutchart.php
+++ widgets/piedonutchart.php
@@ -682,8 +682,9 @@ class MT_piedonutchart extends Widget_Base {
         }
         $this->add_render_attribute( 'mighty-chart-position', 'class', 'mighty-chart' );
 
-        $this->add_render_attribute( 'mighty-chart', 'id', 'canvas' );
+        $this->add_render_attribute( 'mighty-chart', 'id', 'canvas'.$this->get_id() );
         $this->add_render_attribute( 'chart_title', 'class', 'chart-title' );
+        $this->add_render_attribute( 'mighty-chart', 'data-id', $this->get_id() );
         $this->add_render_attribute( 'mighty-chart', 'data-label', json_encode($label) );
         $this->add_render_attribute( 'mighty-chart', 'data-values', json_encode($values) );
         $this->add_render_attribute( 'mighty-chart', 'data-borderColor', json_encode($borderColor) );
